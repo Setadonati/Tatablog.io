c56d9288747e9aedda030f1188154387e980fd60
SETA DONATI <96702733+Setadonati@users.noreply.github.com>
2024-01-12 12:15:03 +0300

    myblog

---
+++ assets/js/admin.js
@@ -0,0 +1,12 @@
+// Sidebar responsiveness
+const menuIcon = document.querySelector('.menu-icon');
+const sidebar = document.querySelector('.sidebar');
+const navOverlay = document.querySelector('.nav-overlay');
+
+function toggleSidebar() {
+  sidebar.classList.toggle('open');
+  navOverlay.classList.toggle('open');
+}
+
+menuIcon.addEventListener('click', toggleSidebar);
+navOverlay.addEventListener('click', toggleSidebar);
